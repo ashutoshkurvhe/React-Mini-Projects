1297a96242fa367043f6d130ad72ba571c8411fe
ashutosh <kurvheashutosh@gmail.com>
2025-03-22 15:36:46 +0530

    Todo List

--- Todo List/src/App.jsx
+++ Todo List/src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import AppName from "./Components/AppName";
+import TodoInputs from "./Components/TodoInputs";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="container">
+      <AppName></AppName>
+      <TodoInputs></TodoInputs>
+    </div>
+  );
 }
 
-export default App
+export default App;
